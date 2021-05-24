a0a6ce67ccf83b1b69680586efa75884ddfcfea0
Giulia Maia <gmaiaserafim@gmail.com>
2021-05-24 18:35:07 -0300

    Ambiente configurado de acordo com as especificações pedidas no docs(maven-archetype-webapp, java 8, wildfly 11), classes Produto e Usuario criadas e integração com banco de dados usando hibernate feita.

---
+++ projeto-web/src/main/java/dominio/Usuario.java
@@ -0,0 +1,72 @@
+package dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private String email;
+	private String senha;
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario(int id, String nome, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
+	}
+	
+	
+	
+}
